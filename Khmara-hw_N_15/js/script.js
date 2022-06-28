5f8e3c11de8fa732969cd98b82cad5a4904982e4
Ekaterina <ekaterinakhmara01@gmail.com>
2022-06-28 21:33:47 +0300

    delete console log

--- Khmara-hw_N_15/js/script.js
+++ Khmara-hw_N_15/js/script.js
@@ -13,15 +13,12 @@ function calc(result){
         result = result * munMult;
       },
       getResult: () => {
-        console.log(result);
+        return result;
       }
   }
 }
   
 const first = calc(4);
-  
-console.log("object return = ");
-console.log(first);
 
 first.add(33);
 first.sub(12);
@@ -30,45 +27,4 @@ first.mult(4);
 
 first.getResult();
 
-
-// Еще перечитала, не сразу поняла что конкретно нужно делать в getResult и как оно ддолжно работать,
-//  десяток раз поисправляла, и пришла к тому что выше
-
-// Было так:
-
-// function calc(a){
-//   const add = function(b){
-//     return b + a
-//   }
-//   const sub = function(c){
-//     return c - a
-//   }
-//   const div = function(d){
-//     return d / a
-//   }
-//   const mult = function(e){
-//     return e * a
-//   }
-//   const getResult = function(a,b,c,d,e){
-//     return (((a+b)-c)/d)*e
-//   }
-//   return {
-//     add,
-//     sub,
-//     div,
-//     mult,
-//     getResult
-//   }
-// }
-
-// const first = calc(4);
-
-// console.log("object return = ");
-// console.log(first);
-
-// console.log("add: 33 + 4 = " + first.add(33));
-// console.log("sub: 12 - 4 = " + first.sub(12));
-// console.log("div: 2 / 4 = " + first.div(2));
-// console.log("mult: 4 * 4 = " + first.mult(4));
-
-// console.log("getResult: (((4 + 33) - 12) / 2) * 4 = " + first.getResult(4,33,12,2,4));
+console.log(first.getResult());
